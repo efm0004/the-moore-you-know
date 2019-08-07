6bb646933540edaaf545c3490dbec2f9677fcd66
Emily Moore <emilymoore@Emilys-MacBook-Air.local>
2019-08-06 20:13:58 -0500

    fixed handleClick function, abandoned faceImage/nameImage functions

--- js/main.js
+++ js/main.js
@@ -121,25 +121,41 @@ let deck = [
         cardType: "name"
     }]
 
-    var cardBack = "TMYK-Photos/TMYK-back.png";
+var cardBack = "TMYK-Photos/TMYK-back.png";
 
-    var cardElements = {
+    // var cardElements = {
 
-    }
+    // }
 
 
-// var winning = [] //to figure out logic
+var matches = [
+    [0, 1],
+    [2, 3],
+    [4, 5],
+    [6, 7],
+    [8, 9],
+    [10, 11],
+    [12, 13],
+    [14, 15],
+    [16, 17],
+    [18, 19],
+    [20, 21],
+    [22, 23],
+]
+
+// var winning = 12; 
 
 /*----- app's state (variables) -----*/ 
 let cardsInPlay = [];
-let firstCard, secondCard, win;
+let random = [];
+let win;
 
 /*----- cached element references -----*/ 
 let image = document.querySelectorAll("img");
-
+let board = document.querySelector('.board'); 
 
 /*----- event listeners -----*/ 
-document.querySelector(".board").addEventListener('click', handleClick)
+board.addEventListener('click', handleClick);
 
 
 /*----- functions -----*/
@@ -148,7 +164,7 @@ init();
 
 function init() {
     cardsInPlay = [];
-    win = null; 
+    winning = 0; 
     shuffle ();
     render ();
 }
@@ -158,62 +174,98 @@ function init() {
 //evt listener that does an onClick
 
 function render() {
-    
+    //create an image tag; give tag scr of back; give it class attribute for first
+    //argument, and then class attribute as second argument
+    random.forEach(function(el, idx) {
+        let img = document.createElement('img');
+        img.setAttribute('class', `back card face`);
+        img.setAttribute('src', 'TMYK-Photos/TMYK-back.png');
+        img.setAttribute('id', `${el}`);
+        board.appendChild(img);
+    })
 }
 
 function shuffle () {
     var max = 24; 
-    var random = [];
     for (var i = 0; i<max; i++){
         var temp = Math.floor(Math.random()*max);
         if (random.indexOf(temp) === -1){
             random.push(temp);
         } else (i--);
-    }
+    } console.log(random);
     // const setRandomImages;
     let images = document.querySelectorAll('img.back')
     images.forEach((imageEl, index) => {
-        console.log(deck[random[index]])
+        // console.log(deck[random[index]])
         // imageEl.setAttribute('src', deck[random[index]].cardImage)
         imageEl.setAttribute('class', `back ${deck[random[index]].name}`)
-        console.log(imageEl);
+        // console.log(imageEl);
     })
 }
 
 function handleClick(evt){
-    let tClass = evt.target.className;
-    if (tClass.includes("card")) {
-        if (tClass.includes("back")) {
-            let faceImage = getFaceImage(tClass);
-            let nameImage = getNameImage(tClass);
-            console.log(faceImage);
-            console.log(nameImage);
-        } else console.log("face up");
+    var evtTarget = evt.target;
+    console.log(evt.target.id);
+    evtTarget.setAttribute('src', `${deck[evt.target.id].cardImage}`);
+    cardsInPlay.push({id:evt.target.id, src: evt.target.src});
+    getMatch(); //need to find a way to delay this - the second cardsInPlay value doesn't change image
+    // if (tClass.includes("card")) {
+    //     if (tClass.includes("back")) {
+    //         let faceImage = getFaceImage(tClass);
+    //         let nameImage = getNameImage(tClass);
+    //         console.log(faceImage);
+    //         console.log(nameImage);
+    //     } else console.log("face up");
+    // }
+}
+
+function getMatch() {
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert('You found a match!');
+            cardsInPlay.length = 0;
+            winning += 1; 
+        } else {
+            alert('Sorry, try again');
+            document.getElementById(cardsInPlay[0].id)
+                .setAttribute('src', `${cardBack}`);
+            document.getElementById(cardsInPlay[1].id)
+                .setAttribute('src', `${cardBack}`);
+            cardsInPlay.length = 0;
+            console.log(cardsInPlay);
+            // cardsInPlay.forEach(function(idx) {
+            //     image.setAttribute('src', 'TMYK-Photos/TMYK-back.png');
+            //     console.log(image)
+            //     cardsInPlay.length = 0;
+            // }
+            
+            // cardsInPlay.forEach(function(el, idx).setAttribute('src', `${cardBack}`);
+        }
     }
 }
 
-function getFaceImage(tClass){ 
-    //get class in card that's clicked and then change image to correct URL
-    tClass = tClass.replace("back ", "")
-    let personImage;
-    deck.forEach(person => {
-        if (person.name === tClass && person.cardType === "face")
-        personImage = person.cardImage;
-    }); 
-    return personImage;
-} 
+// function getFaceImage(tClass){ 
+//     //get class in card that's clicked and then change image to correct URL
+//     tClass = tClass.replace("back ", "")
+//     let personImage;
+//     deck.forEach(person => {
+//         if (person.name === tClass && person.cardType === "face")
+//         personImage = person.cardImage;
+//     }); 
+//     return personImage;
+// } 
 
-function getNameImage(tClass){
-    tClass = tClass.replace("back ", "")
-    let nameImage;
-    deck.forEach(person => {
-        if (person.name === tClass && person.cardType === "name")
-        nameImage = person.cardImage;
-    }); 
-    return nameImage;
-}
+// function getNameImage(tClass){
+//     tClass = tClass.replace("back ", "")
+//     let nameImage;
+//     deck.forEach(person => {
+//         if (person.name === tClass && person.cardType === "name")
+//         nameImage = person.cardImage;
+//     }); 
+//     return nameImage;
+// }
 
-function flipCard() {
-    var cardId = this.getFaceImage(tClass);
-    this.setAttribute('src', cardId);
-}
+// function flipCard() {
+//     var cardId = this.getFaceImage(tClass);
+//     this.setAttribute('src', cardId);
+// }
